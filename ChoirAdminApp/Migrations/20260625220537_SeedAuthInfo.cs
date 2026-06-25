f565e6a71330d880d4c61a6804cd03be8273a973
Viviana Luongo <viviana.luongo@nortal.com>
2026-06-25 19:52:45 -0300

    Add SeedAuthInfo migration

---
+++ ChoirAdminApp/Migrations/20260625220537_SeedAuthInfo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ChoirAdminApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAuthInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "RoleId", "RoleName" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), "Admin" },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), "Read" },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), "Create" },
+                    { new Guid("44444444-4444-4444-4444-444444444444"), "Edit" },
+                    { new Guid("55555555-5555-5555-5555-555555555555"), "Delete" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "PasswordHash", "Username" },
+                values: new object[] { new Guid("66666666-6666-6666-6666-666666666666"), "PLACEHOLDER", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "RoleID", "UserID" },
+                values: new object[] { new Guid("11111111-1111-1111-1111-111111111111"), new Guid("66666666-6666-6666-6666-666666666666") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"));
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: new Guid("55555555-5555-5555-5555-555555555555"));
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumns: new[] { "RoleID", "UserID" },
+                keyValues: new object[] { new Guid("11111111-1111-1111-1111-111111111111"), new Guid("66666666-6666-6666-6666-666666666666") });
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("66666666-6666-6666-6666-666666666666"));
+        }
+    }
+}
