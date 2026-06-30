668bcbf0401033ff66a3765adcc2b4f080cc8679
Viviana Luongo <viviana.luongo@nortal.com>
2026-06-30 00:32:18 -0300

    Update choir dtos

--- ChoirAdminApp/Dtos/Choir/PostChoirDto.cs
+++ ChoirAdminApp/Dtos/Choir/PostChoirDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChoirAdminApp.Dtos.Choir
 {
-	public class SetChoirDto
+	public class PostChoirDto
 	{
 		public string Name { get; set; } = string.Empty;
 		public string Description { get; set; } = string.Empty;
---
+++ ChoirAdminApp/Dtos/Choir/PutChoirDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChoirAdminApp.Dtos.Choir
+{
+	public class PutChoirDto
+	{
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+		public Guid? Director { get; set; }
+		public List<Guid>? Chorists { get; set; }
+	}
+}
